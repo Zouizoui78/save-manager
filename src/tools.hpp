19addd603ea9ffaec80711fc7fa42ca71794520a
Alexandre Zouiten <alexandre.zouiten1@gmail.com>
2023-07-20 08:13:57 +0200

    cleanup

--- src/tools.hpp
+++ src/tools.hpp
@@ -8,10 +8,6 @@ namespace tools {
 
 std::vector<std::filesystem::path> list_files_from_directory(const std::string& directory);
 
-uint32_t get_save_number(const std::string &filename);
-
-bool is_manual_save(const std::string &save_name);
-
 }
 
 #endif // TOOLS_HPP
